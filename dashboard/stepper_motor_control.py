6181e5bc5063b6ed6db2716ceee90f43506dcaff
shaileyjoseph <shaileyjoseph2006@gmail.com>
2025-10-08 20:14:56 -0700

    changed pin numbers back

--- dashboard/stepper_motor_control.py
+++ dashboard/stepper_motor_control.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 # The numbers are the pun numbers of the GPIO pins on the pi
-DIR = 17 #direction, 0 = CCW, 1 = CW
-CLK = 27 #pulses when set to 0 + moves 1 step
-ENA = 22 #0 = off, 1 = on
+DIR = 11 #direction, 0 = CCW, 1 = CW
+CLK = 13 #pulses when set to 0 + moves 1 step
+ENA = 15 #0 = off, 1 = on
 SOL = 18 #controls solenoid, 0 = down, 1 = up? idk it might be the opposite
 delay = 0.000001 # the clock pulses + time between them has to be this many seconds minimum
 linear_actuator_delay = 1 #idk
@@ -11,7 +11,7 @@ linear_actuator_delay = 1 #idk
 trash = 40 # of steps to take
 recycling = 80
 compost = 120
-stuff = {"trash": trash, "recycling":recycling, "compost": compost}
+stuff = {"trash": (trash, 1), "recycling":(recycling, 1), "compost": (compost, 1)}
 
 def initialize_gpio():
     GPIO.setmode(GPIO.BCM) #as opposed to GPIO.BCM, which uses a different pin numbering scheme
@@ -19,10 +19,10 @@ def initialize_gpio():
       GPIO.setup(pin, GPIO.OUT)
     GPIO.setwarnings(False)
 
-def move(category, direction):
+def move(steps, direction):
     GPIO.output(ENA, 1) # turn it on
     GPIO.output(DIR, direction) # set direction
-    for x in range(stuff[category]):
+    for x in range(steps):
         GPIO.output(CLK, 1)
         time.sleep(delay)
         GPIO.output(CLK, 0)
@@ -38,10 +38,10 @@ def main():
         try:
             category = input() #replace this with function for receiving actual classification, should also
                           #wait on this line
-            move(category, 1)
+            move(*stuff[category])
             #something with the linear actuator
             time.sleep(1) #placeholder, we need a delay for motor to change direction probably
-            move(category, 0)
+            move(stuff[category][0], not stuff[category][1])
         except KeyboardInterrupt:
             GPIO.cleanup()
             exit()
